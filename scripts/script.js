f28bf36dfbe973506099288a61f3b73e7ed01192
danyamamin99 <danya.mamin.99@bk.ru>
2020-07-09 15:22:40 +0300

    2 день

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,80 @@
 'use strict';
 
- const modalAdd = document.querySelector('.modal__add'),
+// 1 день
+// Добавили открытие 2 модалок и закрытие 2 модалок, перезагрузка формы,
+// блокировка кнопки отправить на форме, закрытие модалок с помощью ESC
+// 2 день
+// Сделали функцию closeModal и closeModalEsc, использовал метод spread, проверку валидации формы
+// создали константу dataBase, добавили событие форме('submit')
+
+const dataBase = [];
+
+const modalAdd = document.querySelector('.modal__add'),
       addAd = document.querySelector('.add__ad'),
       modalBtnSubmit = document.querySelector('.modal__btn-submit'),
       modalSubmit = document.querySelector('.modal__submit'),
       catalog = document.querySelector('.catalog'),
-      modalItem = document.querySelector('.modal__item');
-// Добавление объявления и блокировки кнопки отправки формы
+      modalItem = document.querySelector('.modal__item'),
+      modalBtnWarning = document.querySelector('.modal__btn-warning');
+
+// Метод spread(...) коллекция разбивает на элементы, затем все элементы мы засовывает в массив, потом фильтруем элементы, чтоб не было BUTTON
+const elementsModalSubmit = [...modalSubmit.elements].filter((elem) => elem.tagName !== 'BUTTON');
+
+// Функция закрытия модального окна
+const closeModal = function(event) {
+  const target = event.target;
+
+  if (target.closest('.modal__close') || target === this) {
+    this.classList.add('hide');
+    if (this === modalAdd) {
+      modalSubmit.reset();
+    }
+  }
+};
+// Функия закрытия модального окна через Esc
+const closeModalEsc = (event) => {
+  if (event.code === 'Escape') {
+    modalAdd.classList.add('hide');
+    modalItem.classList.add('hide');
+    modalSubmit.reset();
+    document.body.removeEventListener('keydown', closeModalEsc);
+  }
+};
+// Открытие модалки -добавления объявления и блокировки кнопки формы
 addAd.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
   modalBtnSubmit.disabled = true;
-});
-// Закрытие модалки с помощью крестика и фона, перезагрузка формы
-modalAdd.addEventListener('click', (event) => {
-  const target = event.target;
-
-  if (target.classList.contains('modal__close') || target === modalAdd) {
-    modalAdd.classList.add('hide');
-    modalSubmit.reset();
-  }
-});
-// Закрытие модалки с помощью ESC и перезагрузка формы
-document.body.addEventListener('keydown', (event) => {
-  if (event.keyCode === 27) {
-    modalAdd.classList.add('hide');
-    modalSubmit.reset();
-    modalItem.classList.add('hide');
-  }
+  document.body.addEventListener('keydown', closeModalEsc);
 });
 // Открытие модалки -  товара
 catalog.addEventListener('click', (event) => {
   const target = event.target;
-  
+
   if (target.closest('.card')) {
     modalItem.classList.remove('hide');
+    document.body.addEventListener('keydown', closeModalEsc);
   }
 });
+// Закрытие модалки с помощью крестика и фона, перезагрузка формы
+modalAdd.addEventListener('click', closeModal);
 // Закрытие модалки - товара
-modalItem.addEventListener('click', (event) => {
-  const target = event.target;
-
-  if (target.classList.contains('modal__close') || target === modalItem) {
-    modalItem.classList.add('hide');
-  }
+modalItem.addEventListener('click', closeModal);
+// Проверка валидации формы, разблокировка кнопки и убирание текста
+// Every возвращает true если все элементы не пустые
+modalSubmit.addEventListener('input', () => {
+  const validForm = elementsModalSubmit.every(elem => elem.value);
+  modalBtnSubmit.disabled = !validForm;
+  modalBtnWarning.style.display = validForm ? 'none' : '';
 });
-// ghbdtn
+// Отправка формы
+modalSubmit.addEventListener('submit',(event) => {
+  event.preventDefault();
+  const itemObj = {};
+
+  for (const elem of elementsModalSubmit) {
+    itemObj[elem.name] = elem.value;
+  }
+
+  dataBase.push(itemObj);
+  console.log(dataBase);
+});
